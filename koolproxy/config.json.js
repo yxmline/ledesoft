eed7bb1211490d42dcbea5a26088fc145874ec7b
houzi- <wojiaolinmu008@gmail.com>
2018-10-02 21:45:12 +0800

    update version <koolproxy>

--- koolproxy/config.json.js
+++ koolproxy/config.json.js
@@ -1,9 +1,9 @@
 {
-"version":"3.8.3.1",
-"md5":"577db6c48fc59d3e9202ca439ab8c687",
+"version":"3.8.3.2",
+"md5":"6062f1d41beb4b79088472e1ca9a6143",
 "home_url":"Module_koolproxy.asp",
 "title":"koolproxy",
 "description":"听说KP和软路由更搭哦~",
-"changelog":"修复离线安装和主过滤模式BUG",
-"build_date":"2018-10-01_20:25:37"
+"changelog":"优化配置界面",
+"build_date":"2018-10-02_21:44:45"
 }
--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -503,15 +503,15 @@
             "version": "0.2"
         }, 
         {
-            "build_date": "2018-10-01_20:25:37", 
-            "changelog": "修复离线安装和主过滤模式BUG", 
+            "build_date": "2018-10-02_21:44:45", 
+            "changelog": "优化配置界面", 
             "description": "听说KP和软路由更搭哦~", 
             "home_url": "Module_koolproxy.asp", 
-            "md5": "577db6c48fc59d3e9202ca439ab8c687", 
+            "md5": "6062f1d41beb4b79088472e1ca9a6143", 
             "name": "koolproxy", 
             "tar_url": "koolproxy/koolproxy.tar.gz", 
             "title": "koolproxy", 
-            "version": "3.8.3.1"
+            "version": "3.8.3.2"
         }, 
         {
             "build_date": "2017-10-09_22:23:58", 
