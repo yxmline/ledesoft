efda113ac1eba28423edbc2e7697deec29db4e42
houzi- <wojiaolinmu008@gmail.com>
2018-10-14 11:04:17 +0800

    update version

--- koolss/build.sh
+++ koolss/build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 MODULE=koolss
-VERSION="2.1.3"
+VERSION="2.1.4"
 TITLE=科学上网插件
 DESCRIPTION="轻松科学上网~"
 HOME_URL=Module_koolss.asp
-CHANGELOG="优化iptables"
+CHANGELOG="修复BUG"
 
 # Check and include base
 DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
